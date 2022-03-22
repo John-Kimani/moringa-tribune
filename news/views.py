0414c0bb0317b98c53654a40fcc414e3acfe982b
John-Kimani <kimwaweru100@gmail.com>
2022-03-22 11:05:12 +0300

    Dev: Creates day of the week function

--- news/views.py
+++ news/views.py
@@ -9,11 +9,23 @@ def welcome(request):
 
 def news_of_day(request):
     date = dt.date.today()
+    # function to convert date object to find exact day
+    day = convert_dates(date)
     html = f'''
             <html>
             <body>  
-                <h1> {date.day}-{date.month}-{date.year}</h1>
+                <h1> News for {day} {date.day}-{date.month}-{date.year}</h1>
             </body>
             </html>
             '''
     return HttpResponse(html)
+
+
+def convert_dates(dates):
+    #Function that gets the weekday number for the date.
+    day_number = dt.date.weekday(dates)
+    days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+
+    #retuning the actual day of the week
+    day = days[day_number]
+    return day
