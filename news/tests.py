9197fb0e7cf114ad03f282b3ca24caea9011e3ce
John-Kimani <kimwaweru100@gmail.com>
2022-03-23 18:13:53 +0300

    Test editor class pass

--- news/tests.py
+++ news/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
 
 # Create your tests here.
+from .models import Editor, Article, tags
+
+
+class EditorTestClass(TestCase):
+
+    # set up method
+    def setUp(self):
+        self.kimani = Editor(first_name='Kimani', last_name='John', email='Kim@john.com')
+    
+    # testing instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.kimani, Editor))
