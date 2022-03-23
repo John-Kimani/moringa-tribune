7d6ca90849fb60759e525e7d32cafa48e21da721
John-Kimani <kimwaweru100@gmail.com>
2022-03-23 17:52:58 +0300

    Creates database relationships

--- news/models.py
+++ news/models.py
@@ -8,3 +8,27 @@ class Editor(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     email = models.EmailField()
+
+    def __str__(self):
+        # try:
+        #     editor = Editor.objects.get(email='example@gmail.com')
+        #     print('Editor not found')
+        # except DoesNotExist:
+        #     print('Editor was not found')
+
+        return self.first_name
+    class Meta:
+        ordering = ['first_name']
+
+class tags(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name    
+
+class Article(models.Model):
+    title = models.CharField(max_length=60)
+    post = models.TextField()
+    editor = models.ForeignKey(Editor)
+    tags = models.ManyToManyField(tags)
+    pub_date = models.DateTimeField(auto_now_add=True)
