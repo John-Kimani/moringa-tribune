16d5cd8919c789f30d0bbb530941696bafecfd06
John-Kimani <kimwaweru100@gmail.com>
2022-03-24 10:36:44 +0300

    Set images upload to admin dashboard

--- news/urls.py
+++ news/urls.py
@@ -1,6 +1,8 @@
 
 from django.urls import re_path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns=[ 
@@ -9,3 +11,6 @@ urlpatterns=[
      re_path('^archives/(\d{4}-\d{2}-\d{2})/$', views.past_days_news, name='pastNews'),
      re_path('^search/', views.search_results, name='search_results')
 ]
+
+if settings.DEBUG:
+     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
