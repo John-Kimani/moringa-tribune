2d5cab6ab25d2f7cf1b5eef58df3e4937863d00e
John-Kimani <kimwaweru100@gmail.com>
2022-04-07 00:45:20 +0300

    Dev: enables User authentication

--- tribune/urls.py
+++ tribune/urls.py
@@ -15,9 +15,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+# from news import views
+# from django.contrib.auth import views as auth_views
+from django.contrib.auth import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('news.urls')),
+
+
+    path('accounts/', include('django_registration.backends.one_step.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+
+    path('logout/', views.LogoutView.as_view(), {"next_page":'/'})
 ]
